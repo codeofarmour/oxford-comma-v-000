24e139bf7f866516ec41abdeb6c32cb3a64532b6
Terri Riggle <codeofarmour@outlook.com>
2017-10-06 12:27:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,11 @@
 def oxford_comma(array)
-  array.length
-  array[-1].insert(0, "and")
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else array.length > 2
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+  end
 end
-  array.join(",")
-end
+    
